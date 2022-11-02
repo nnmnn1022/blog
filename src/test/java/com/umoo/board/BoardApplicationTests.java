fef4e1fcff281dda9af4c88622b02cb6ab50c9ce
Umoo <nnmnn1022@naver.com>
2022-11-02 23:10:15 +0900

    profile 분리 및 staging 테스트

--- src/test/java/com/umoo/board/BoardApplicationTests.java
+++ src/test/java/com/umoo/board/BoardApplicationTests.java
@@ -1,13 +1,25 @@
 package com.umoo.board;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class BoardApplicationTests {
+	@Autowired
+	Environment environment;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void 프로필_적용_여부(){
+		String[] activeProfiles = environment.getActiveProfiles();
+		assertThat(activeProfiles).containsExactly("prod");
 	}
 
 }
