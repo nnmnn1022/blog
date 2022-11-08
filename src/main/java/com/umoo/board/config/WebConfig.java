fd07a643cb0f09d90bb503543a55da61ddd3447f
Umoo <nnmnn1022@naver.com>
2022-11-08 22:36:00 +0900

    CKEditor 이미지 업로드 작업 중

--- src/main/java/com/umoo/board/config/WebConfig.java
+++ src/main/java/com/umoo/board/config/WebConfig.java
@@ -4,19 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
-public class webConfig {
-
+@Configuration
+public class WebConfig {
     /**
      * CKFinder의 return : json
      * uploaded, url을 포함하고 있음.
      * controller가 modelandview를 통해 json 형식으로 return 할 수 있도록 설정
-      */
-    @Configuration
-    public class WebConfig {
-
-        @Bean
-        MappingJackson2JsonView jsonView() {
-            return new MappingJackson2JsonView();
-        }
+     */
+    @Bean
+    MappingJackson2JsonView jsonView() {
+        return new MappingJackson2JsonView();
     }
 }
