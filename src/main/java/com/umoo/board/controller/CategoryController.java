f890f6c216b31313e1c625bc2e5320255037f5c2
Umoo <nnmnn1022@naver.com>
2022-12-10 23:17:14 +0900

    카테고리 CRUD 구현 중

--- src/main/java/com/umoo/board/controller/CategoryController.java
+++ src/main/java/com/umoo/board/controller/CategoryController.java
@@ -1,12 +1,16 @@
 package com.umoo.board.controller;
 
+import com.umoo.board.entity.Category;
 import com.umoo.board.service.CategoryService;
 import com.umoo.board.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class CategoryController {
@@ -17,6 +21,12 @@ public class CategoryController {
     /**
      * 카테고리 조회
      */
+    @GetMapping("/category/list") //blog.umoo.pe.kr/
+    public String Category() {
+        List<Category> list = categoryService.list();
+
+        return "redirect:article/list";
+    }
 
     /**
      * 카테고리 추가
